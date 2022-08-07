dfdc553c63137c08cd648b5469c01ac854a8b010
izofar <izofar43@gmail.com>
2022-08-06 22:00:36 -0500

    v1.2.1 (i) - Added stone axe to corpor spawn - Added shield to wither skeleton knight spawn - removed sword from wraither when dispossessed - Added block drops for withered and warped blocks - Fixed broken deepslate recipes by correcting chiseled recipes - Added russian localization - Updated banner - Updated versioning

--- src/main/java/com/izofar/bygonenether/entity/Wraither.java
+++ src/main/java/com/izofar/bygonenether/entity/Wraither.java
@@ -1,6 +1,10 @@
 package com.izofar.bygonenether.entity;
 
 import com.izofar.bygonenether.init.ModEntityTypes;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
@@ -13,14 +17,51 @@ import net.minecraft.world.level.Level;
 
 public class Wraither extends WitherSkeleton {
 
+    private static final EntityDataAccessor<Boolean> DATA_IS_POSSESSED = SynchedEntityData.defineId(Wraither.class, EntityDataSerializers.BOOLEAN);
+
     public Wraither(EntityType<? extends WitherSkeleton> entityType, Level level) { super(entityType, level); }
 
     public static AttributeSupplier.Builder createAttributes() {
-        return Monster.createMonsterAttributes().add(Attributes.MOVEMENT_SPEED, 0.35D).add(Attributes.MAX_HEALTH, 10.0D);
+        return Monster.createMonsterAttributes().add(Attributes.MOVEMENT_SPEED, 0.35D).add(Attributes.MAX_HEALTH, 25.0D);
     }
 
     @Override
-    public void die(DamageSource source){
+    public void addAdditionalSaveData(CompoundTag tag){
+        super.addAdditionalSaveData(tag);
+        tag.putBoolean("Possessed", this.isPossessed());
+    }
+
+    @Override
+    public void readAdditionalSaveData(CompoundTag tag){
+        super.readAdditionalSaveData(tag);
+        this.setPossessed(tag.getBoolean("Possessed"));
+    }
+
+    @Override
+    protected void defineSynchedData(){
+        super.defineSynchedData();
+        this.entityData.define(DATA_IS_POSSESSED, true);
+    }
+
+    public boolean isPossessed(){
+        return this.entityData.get(DATA_IS_POSSESSED);
+    }
+
+    private void setPossessed(boolean possessed){
+        this.entityData.set(DATA_IS_POSSESSED, possessed);
+    }
+
+    @Override
+    public boolean hurt(DamageSource source, float amount){
+        boolean ret = super.hurt(source, amount);
+        if(this.isPossessed() && this.getHealth() < 10.0F)
+            this.dispossess();
+        return ret;
+    }
+
+    private void dispossess(){
+        this.setPossessed(false);
+        this.getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue(0.25D);
         if(this.getLevel() instanceof ServerLevel serverLevel) {
             Wex wex = ModEntityTypes.WEX.get().create(serverLevel);
             wex.moveTo(this.blockPosition().above(), this.yBodyRot, this.xRotO);
@@ -28,6 +69,5 @@ public class Wraither extends WitherSkeleton {
             wex.setPersistenceRequired();
             serverLevel.addFreshEntity(wex);
         }
-        this.convertTo(EntityType.WITHER_SKELETON, true);
     }
 }
