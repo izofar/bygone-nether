ea6f2c9871e20c9d8d92de74ed9329e088ddd6f7
izofar <izofar43@gmail.com>
2023-02-25 15:45:05 -0600

    v1.2.2 (ii) - Code reorganization after branch merges

--- src/main/java/com/izofar/bygonenether/mixin/ModifyDamageInSmithingRecipes.java
+++ src/main/java/com/izofar/bygonenether/mixin/ModifyDamageInSmithingRecipes.java
@@ -1,6 +1,7 @@
 package com.izofar.bygonenether.mixin;
 
 import com.izofar.bygonenether.init.ModItems;
+import com.izofar.bygonenether.item.ModArmorItem;
 import net.minecraft.nbt.IntTag;
 import net.minecraft.world.Container;
 import net.minecraft.world.item.ItemStack;
@@ -19,7 +20,7 @@ public class ModifyDamageInSmithingRecipes {
     )
     private void bygonenether_setDamage(Container container, CallbackInfoReturnable<ItemStack> cir) {
         ItemStack itemstack = cir.getReturnValue();
-        if(!(itemstack.getItem() == ModItems.GILDED_NETHERITE_HELMET || itemstack.getItem() == ModItems.GILDED_NETHERITE_CHESTPLATE || itemstack.getItem() == ModItems.GILDED_NETHERITE_LEGGINGS || itemstack.getItem() == ModItems.GILDED_NETHERITE_BOOTS)) return;
+        if(!(itemstack.getItem() instanceof ModArmorItem)) return;
         int damage = itemstack.getDamageValue();
         itemstack.getOrCreateTag().put("NetheriteDamage", IntTag.valueOf(damage));
         itemstack.setDamageValue(0);
