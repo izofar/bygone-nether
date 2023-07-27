3d1b2e770910c6c46ba5129ade32a60949f85ee9
izofar <izofar43@gmail.com>
2023-07-27 17:45:45 -0500

    v1.3.0 (iii) - Restored deleted classes that were, in fact, necessary - Restored resource jsons that were, in fact, necessary - Substituted block in piglin manor not present in 1.16.5

--- src/main/java/com/izofar/bygonenether/client/model/PiglinHunterModel.java
+++ src/main/java/com/izofar/bygonenether/client/model/PiglinHunterModel.java
@@ -13,12 +13,22 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
-public class PiglinHunterModel extends PiglinModel<PiglinHunterEntity> {
+public class PiglinHunterModel extends ModPlayerModel<PiglinHunterEntity> {
 
     public final ModelRenderer hoglinSkull;
+    public final ModelRenderer earRight;
+    public final ModelRenderer earLeft;
+    public final ModelRenderer bodyDefault;
+    private final ModelRenderer headDefault;
+    public final ModelRenderer leftArmDefault;
+    public final ModelRenderer rightArmDefault;
 
     public PiglinHunterModel(float scale, int texWidth, int texHeight) {
         super(scale, texWidth, texHeight);
+        this.texWidth = texWidth;
+        this.texHeight = texHeight;
+        this.body = new ModelRenderer(this, 16, 16);
+        this.body.addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, scale);
         hoglinSkull = new ModelRenderer(this);
         hoglinSkull.setPos(0.0F, 15.5F, 25.0F);
         body.addChild(hoglinSkull);
@@ -26,12 +36,66 @@ public class PiglinHunterModel extends PiglinModel<PiglinHunterEntity> {
         hoglinSkull.texOffs(61, 1).addBox(-7.0F, 16.5F, 0.0F, 14.0F, 6.0F, 19.0F, -0.9F, false);
         hoglinSkull.texOffs(61, 3).addBox(5.0F, 11.5F, 0.0F, 2.0F, 11.0F, 2.0F, 0.0F, false);
         hoglinSkull.texOffs(70, 3).addBox(-7.0F, 11.5F, 0.0F, 2.0F, 11.0F, 2.0F, 0.0F, false);
+        this.head = new ModelRenderer(this);
+        this.head.texOffs(0, 0).addBox(-5.0F, -8.0F, -4.0F, 10.0F, 8.0F, 8.0F, scale);
+        this.head.texOffs(31, 1).addBox(-2.0F, -4.0F, -5.0F, 4.0F, 4.0F, 1.0F, scale);
+        this.head.texOffs(2, 4).addBox(2.0F, -2.0F, -5.0F, 1.0F, 2.0F, 1.0F, scale);
+        this.head.texOffs(2, 0).addBox(-3.0F, -2.0F, -5.0F, 1.0F, 2.0F, 1.0F, scale);
+        this.earRight = new ModelRenderer(this);
+        this.earRight.setPos(4.5F, -6.0F, 0.0F);
+        this.earRight.texOffs(51, 6).addBox(0.0F, 0.0F, -2.0F, 1.0F, 5.0F, 4.0F, scale);
+        this.head.addChild(this.earRight);
+        this.earLeft = new ModelRenderer(this);
+        this.earLeft.setPos(-4.5F, -6.0F, 0.0F);
+        this.earLeft.texOffs(39, 6).addBox(-1.0F, 0.0F, -2.0F, 1.0F, 5.0F, 4.0F, scale);
+        this.head.addChild(this.earLeft);
+        this.hat = new ModelRenderer(this);
+        this.bodyDefault = this.body.createShallowCopy();
+        this.headDefault = this.head.createShallowCopy();
+        this.leftArmDefault = this.leftArm.createShallowCopy();
+        this.rightArmDefault = this.leftArm.createShallowCopy();
     }
 
     @Override
     public void setupAnim(PiglinHunterEntity piglinHunterEntity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+        this.body.copyFrom(this.bodyDefault);
+        this.head.copyFrom(this.headDefault);
+        this.leftArm.copyFrom(this.leftArmDefault);
+        this.rightArm.copyFrom(this.rightArmDefault);
         super.setupAnim(piglinHunterEntity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
-
+        float f1 = ageInTicks * 0.1F + limbSwing * 0.5F;
+        float f2 = 0.08F + limbSwingAmount * 0.4F;
+        this.earRight.zRot = (-(float)Math.PI / 6F) - MathHelper.cos(f1 * 1.2F) * f2;
+        this.earLeft.zRot = ((float)Math.PI / 6F) + MathHelper.cos(f1) * f2;
+        PiglinAction piglinaction = ((AbstractPiglinEntity)piglinHunterEntity).getArmPose();
+        if (piglinaction == PiglinAction.DANCING) {
+            float f3 = ageInTicks / 60.0F;
+            this.earLeft.zRot = ((float)Math.PI / 6F) + ((float)Math.PI / 180F) * MathHelper.sin(f3 * 30.0F) * 10.0F;
+            this.earRight.zRot = (-(float)Math.PI / 6F) - ((float)Math.PI / 180F) * MathHelper.cos(f3 * 30.0F) * 10.0F;
+            this.head.x = MathHelper.sin(f3 * 10.0F);
+            this.head.y = MathHelper.sin(f3 * 40.0F) + 0.4F;
+            this.rightArm.zRot = ((float)Math.PI / 180F) * (70.0F + MathHelper.cos(f3 * 40.0F) * 10.0F);
+            this.leftArm.zRot = this.rightArm.zRot * -1.0F;
+            this.rightArm.y = MathHelper.sin(f3 * 40.0F) * 0.5F + 1.5F;
+            this.leftArm.y = MathHelper.sin(f3 * 40.0F) * 0.5F + 1.5F;
+            this.body.y = MathHelper.sin(f3 * 40.0F) * 0.35F;
+        } else if (piglinaction == PiglinAction.ATTACKING_WITH_MELEE_WEAPON && this.attackTime == 0.0F) {
+            this.holdWeaponHigh(piglinHunterEntity);
+        } else if (piglinaction == PiglinAction.CROSSBOW_HOLD) {
+            ModelHelper.animateCrossbowHold(this.rightArm, this.leftArm, this.head, !piglinHunterEntity.isLeftHanded());
+        } else if (piglinaction == PiglinAction.CROSSBOW_CHARGE) {
+            ModelHelper.animateCrossbowCharge(this.rightArm, this.leftArm, piglinHunterEntity, !piglinHunterEntity.isLeftHanded());
+        } else if (piglinaction == PiglinAction.ADMIRING_ITEM) {
+            this.head.xRot = 0.5F;
+            this.head.yRot = 0.0F;
+            if (piglinHunterEntity.isLeftHanded()) {
+                this.rightArm.yRot = -0.5F;
+                this.rightArm.xRot = -0.9F;
+            } else {
+                this.leftArm.yRot = 0.5F;
+                this.leftArm.xRot = -0.9F;
+            }
+        }
         if (piglinHunterEntity.isPassenger()) {
             this.hoglinSkull.y = 10.5F;
         } else {
@@ -61,6 +125,14 @@ public class PiglinHunterModel extends PiglinModel<PiglinHunterEntity> {
             ModelHelper.swingWeaponDown(this.rightArm, this.leftArm, piglinHunterEntity, this.attackTime, ageInTicks);
         } else {
             super.setupAttackAnimation(piglinHunterEntity, ageInTicks);
+        }
+    }
+
+    private void holdWeaponHigh(PiglinHunterEntity entity) {
+        if (entity.isLeftHanded()) {
+            this.leftArm.xRot = -1.8F;
+        } else {
+            this.rightArm.xRot = -1.8F;
         }
     }
 
--- src/main/java/com/izofar/bygonenether/init/ModConfiguredFeatures.java
+++ src/main/java/com/izofar/bygonenether/init/ModConfiguredFeatures.java
@@ -10,9 +10,11 @@ import net.minecraft.world.gen.feature.*;
 
 public abstract class ModConfiguredFeatures {
 
+	// Represented in JSON instead
+	//public static final ConfiguredFeature<?, ?> CONFIGURED_MOB_FEATURE_PIGLIN_MANOR_INSIDE = ModFeatures.MOB_FEATURE_PIGLIN_MANOR_INSIDE.get().configured(IFeatureConfig.NONE);
+	//public static final ConfiguredFeature<?, ?> CONFIGURED_MOB_FEATURE_PIGLIN_MANOR_OUTSIDE = ModFeatures.MOB_FEATURE_PIGLIN_MANOR_OUTSIDE.get().configured(IFeatureConfig.NONE);
+
 	public static final ConfiguredFeature<?, ?> CONFIGURED_MOB_FEATURE_PIGLIN_PRISONER = ModFeatures.MOB_FEATURE_PIGLIN_PRISONER.get().configured(IFeatureConfig.NONE);
-	public static final ConfiguredFeature<?, ?> CONFIGURED_MOB_FEATURE_PIGLIN_MANOR_INSIDE = ModFeatures.MOB_FEATURE_PIGLIN_MANOR_INSIDE.get().configured(IFeatureConfig.NONE);
-	public static final ConfiguredFeature<?, ?> CONFIGURED_MOB_FEATURE_PIGLIN_MANOR_OUTSIDE = ModFeatures.MOB_FEATURE_PIGLIN_MANOR_OUTSIDE.get().configured(IFeatureConfig.NONE);
 	public static final ConfiguredFeature<?, ?> CONFIGURED_MOB_FEATURE_STRIDER = ModFeatures.MOB_FEATURE_STRIDER.get().configured(IFeatureConfig.NONE);
 	public static final ConfiguredFeature<?, ?> CONFIGURED_MOB_FEATURE_WITHER_SKELETON = ModFeatures.MOB_FEATURE_WITHER_SKELETON.get().configured(IFeatureConfig.NONE);
 	public static final ConfiguredFeature<?, ?> CONFIGURED_MOB_FEATURE_CATACOMB = ModFeatures.MOB_FEATURE_CATACOMB.get().configured(IFeatureConfig.NONE);
@@ -22,10 +24,12 @@ public abstract class ModConfiguredFeatures {
 
 	public static void registerConfiguredFeatures() {
 		Registry<ConfiguredFeature<?, ?>> registry = WorldGenRegistries.CONFIGURED_FEATURE;
-		
+
+		// For some reason, these did not work without JSON
+		//Registry.register(registry, new ResourceLocation(BygoneNetherMod.MODID, "mob_feature_piglin_inside"), CONFIGURED_MOB_FEATURE_PIGLIN_MANOR_INSIDE);
+		//Registry.register(registry, new ResourceLocation(BygoneNetherMod.MODID, "mob_feature_piglin_outside"), CONFIGURED_MOB_FEATURE_PIGLIN_MANOR_OUTSIDE);
+
 		Registry.register(registry, new ResourceLocation(BygoneNetherMod.MODID, "mob_feature_piglin_prisoner"), CONFIGURED_MOB_FEATURE_PIGLIN_PRISONER);
-		Registry.register(registry, new ResourceLocation(BygoneNetherMod.MODID, "mob_feature_piglin_inside"), CONFIGURED_MOB_FEATURE_PIGLIN_MANOR_INSIDE);
-		Registry.register(registry, new ResourceLocation(BygoneNetherMod.MODID, "mob_feature_piglin_outside"), CONFIGURED_MOB_FEATURE_PIGLIN_MANOR_OUTSIDE);
 		Registry.register(registry, new ResourceLocation(BygoneNetherMod.MODID, "mob_feature_strider"), CONFIGURED_MOB_FEATURE_STRIDER);
 		Registry.register(registry, new ResourceLocation(BygoneNetherMod.MODID, "mob_feature_wither_skeleton"), CONFIGURED_MOB_FEATURE_WITHER_SKELETON);
 		Registry.register(registry, new ResourceLocation(BygoneNetherMod.MODID, "mob_feature_catacomb"), CONFIGURED_MOB_FEATURE_CATACOMB);
